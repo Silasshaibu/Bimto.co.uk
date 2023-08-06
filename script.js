2ee95cf927fa2f1b7dccb3589338ad1cd3aa93cd
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-08-06 23:31:37 +0100

    Commut

--- script.js
+++ script.js
@@ -2,20 +2,33 @@
 //Header interface on user Logged In state
 //change the default text from log in to profile
 
+
 const headerSwitchs = document.querySelectorAll('.header-switch');
+
 
 //if any of this toggle option is active, then the other should be deactivated
 function toggleLang(){
-    for (i = 0; i < headerSwitchs.length; i++) {
-        const item = headerSwitchs[i];
+    headerSwitchs.forEach((item)=>{
+        const caretDownArrow = item.querySelector('svg');
         const dropDownMenu = item.querySelector('.nested-li');
-        console.log(item);
-
-        // if (item !== otherSelecteditems) {
-        //     console.log(item);
-        // }
-    }
-
+        caretDownArrow.classList.toggle('active');
+        console.log(dropDownMenu);
+        dropDownMenu.classList.toggle('active');
+    })
 }
+
+const body = document.querySelector('body');
+const dropDownMenu = document.querySelector('.nested-li');
+
+    // body.addEventListener('click', ()=>{
+    //     dropDownMenu.style.display ="none";
+    //     console.log('body is clicked');
+    // })
+
+
+
+
+//on click on EN enable the active class of it that is toggle the svg.
+
 
 
