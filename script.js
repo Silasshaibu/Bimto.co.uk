dbcd6a43eb90cc0dfee87172485ff4afaf17e6a0
Silas Shaibu <92685385+Silasshaibu@users.noreply.github.com>
2023-08-07 12:59:53 +0100

    commit

--- script.js
+++ script.js
@@ -2,33 +2,34 @@
 //Header interface on user Logged In state
 //change the default text from log in to profile
 
-
-const headerSwitchs = document.querySelectorAll('.header-switch');
-
-
-//if any of this toggle option is active, then the other should be deactivated
-function toggleLang(){
-    headerSwitchs.forEach((item)=>{
-        const caretDownArrow = item.querySelector('svg');
-        const dropDownMenu = item.querySelector('.nested-li');
-        caretDownArrow.classList.toggle('active');
-        console.log(dropDownMenu);
-        dropDownMenu.classList.toggle('active');
-    })
-}
-
+const searchField = document.querySelector('.miniSearchField');
+const searchIcon = document.querySelector('.searchIcon');
 const body = document.querySelector('body');
-const dropDownMenu = document.querySelector('.nested-li');
 
-    // body.addEventListener('click', ()=>{
-    //     dropDownMenu.style.display ="none";
-    //     console.log('body is clicked');
-    // })
+searchIcon.addEventListener('click',(event)=>{
+    searchField.classList.add('active');
+    event.stopPropagation();
+})
+
+document.addEventListener('click', (event)=>{
+    if(searchField.contains(event.target)){
+    }else{
+        searchField.classList.remove('active');
+    }
+});
 
 
 
-
-//on click on EN enable the active class of it that is toggle the svg.
-
-
+// const headerSwitchs = document.querySelectorAll('.header-switch');
+// //if any of this toggle option is active, then the other should be deactivated
+// function toggleLang(){
+//     headerSwitchs.forEach((item)=>{
+//         const caretDownArrow = item.querySelector('svg');
+//         const dropDownMenu = item.querySelector('.nested-li');
+//         caretDownArrow.classList.toggle('active');
+//         console.log(dropDownMenu);
+//         dropDownMenu.classList.toggle('active');
+//     })
+// }
+// const dropDownMenu = document.querySelector('.nested-li');
 
